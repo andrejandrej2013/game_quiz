673595195ba66b0023a37f454c539beda09c24c8
Andrejs Grinevics <grinevic67@gmail.com>
2022-06-16 17:19:42 +0300

    remove

--- qame_quize/database/migrations/2022_06_10_150944_create_category_table.php
+++ qame_quize/database/migrations/2022_06_10_150944_create_category_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('category', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('category')->unique('category');
+            $table->string('category')->unique();
             $table->timestamps();
         });
     }
